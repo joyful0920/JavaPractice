122c7ed2523649a33c7063de53a83c3774ef7a67
joyful0920 <geunseong0920@gmail.com>
2021-10-07 17:00:19 +0900

    Comment out

--- ch2/src/ch3/OperatorEx6.java
+++ ch2/src/ch3/OperatorEx6.java
@@ -6,8 +6,8 @@ public class OperatorEx6 {
 		
 		byte a = 12;
 		byte b = 22;
-		byte c = a + b; // 에러. byte 끼리의 연산은 int 형으로 자동 변환된 후 연산되기 때문에 결과 값도 int형
-		byte d = (byte) (a + b); // int 형으로 자동 형변환된 값을 byte에 저장하고 있으면 byte형으로 형변환 필요
+		// byte c = a + b; // 에러. byte 끼리의 연산은 int 형으로 자동 변환된 후 연산되기 때문에 결과 값도 int형
+		byte c = (byte) (a + b); // int 형으로 자동 형변환된 값을 byte에 저장하고 있으면 byte형으로 형변환 필요
 		System.out.println(c);
 
 	}
